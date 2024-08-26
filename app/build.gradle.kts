28a0769f18f65d93c9aa1fd264482fda7608ab17
JosueMiguel-cmc <2022338640@ifam.edu.br>
2024-08-26 01:20:51 -0400

    atualizaçao total

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.firebase.common)
-    implementation(libs.firebase.storage)
     implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
